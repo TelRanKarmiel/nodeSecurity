4d3b5bbd97c329055a7617ea4061cdebef920957
Pete Asher <arkhipenkopetr@gmail.com>
2026-01-12 11:53:21 +0200

    demo expired token

--- src/controller/index.ts
+++ src/controller/index.ts
@@ -1,19 +1,23 @@
 import "dotenv/config";
 import JwtUtil from "../security/JwtUtil.ts";
 import { JwtPayload } from "jsonwebtoken";
+import { log } from "console";
 
-const token = JwtUtil.getJwt("vasya12345", "USER")
+const token = JwtUtil.getJwt("vasya12345", "USER", )
+
+setTimeout(()=> verification(token), 60000)
 
 
-const corruptedToken = token.slice(0, 20) + 'g' + token.slice(21)
 
 
-try {
-    const payload = JwtUtil.verifyToken(corruptedToken);
-    console.log(payload);
-} catch (e) {
-    console.log(e.message);
+
+function verification(token:string) {
+    log(token);
+    try {
+        const payload = JwtUtil.verifyToken(token);
+        console.log(payload);
+    } catch (e) {
+        console.log(e.name);
+    }
 }
-
-
 
--- src/security/JwtUtil.ts
+++ src/security/JwtUtil.ts
@@ -1,9 +1,12 @@
 import jwt, { JwtPayload } from "jsonwebtoken";
 
 export default class JwtUtil {
+
     static getJwt(username:string, role:string):string {
-        return jwt.sign({role}, process.env.JWT_SECRET, {subject: username});
+        const expiresIn: any = process.env.TOKEN_EXPIRED_IN || '1h';
+        return jwt.sign({role}, process.env.JWT_SECRET, {subject: username, expiresIn});
     }
+
 
     static verifyToken(token:string):JwtPayload {
         return jwt.verify(token, process.env.JWT_SECRET) as JwtPayload;
