42241beacd5ec7586c077c5a4cdde620d7e00a75
Pete Asher <arkhipenkopetr@gmail.com>
2026-01-13 11:48:18 +0200

    calculator final

--- src/app.ts
+++ src/app.ts
@@ -11,17 +11,6 @@ app.use(express.json());
 app.use("/calc", calc);
 app.use("/auth", auth);
 
-// accountingService.signUp("vasya1234", "12345.com", "USER")
-// accountingService.signUp("petya1234", "12345.com", "USER")
-// accountingService.signUp("vova1234", "12345.com", "USER")
-// accountingService.signUp("kolya1234", "12345.com", "ADMIN")
-// console.time("SignIn in process")
-
-// const tokenVasya = accountingService.signIn("vasya1234", "12345.com");
-
-// const tokenKolya = accountingService.signIn("kolya12341111", "12345.com");
-
-
 
 app.listen(3005, () => {
     console.log("Server started on port 3005");
